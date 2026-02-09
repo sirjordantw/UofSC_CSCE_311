226101ea05e45e0cf52506a839e370a811521d98
sirjordantw <jordantw128@gmail.com>
2026-02-09 03:39:05 +0000

    .

--- CSCE311/proj1/src/main.cc
+++ CSCE311/proj1/src/main.cc
@@ -15,6 +15,13 @@ struct Row{
     std::size_t iterations;
 };
 
+//Structure of shared threads per output of rows.
+struct Shared{
+    int threadID;
+    std::string digest; //The result of several SHA-256 hashes linked.
+    bool finished; //Determines if a row finishes before timeout, and afterwards produces a digest.
+};
+
 //Structure of a thread of that row.
 struct Thread{
     int threadID;
@@ -22,18 +29,20 @@ struct Thread{
     //Shared
     int *currID;
     const std::vector<Row>* rows;
-    const Timings_t* timeout_ms;
+    std::vector<Shared>* out; //Shared outputs per row
+    Timings_t timeout_ms;
 };
+
+CliMode mode;
 
 void* StartRoutine(void* arg);
 
 int main(int argc, char *argv[]){
-    CliMode mode;
     Time_t timeout_ms;
     CliParse(argc, argv, &mode, &timeout_ms);
     int n = get_nprocs();
     std::vector<::pthread_t> thread_pool(n);
-    int currID;
+    int currID = 0;
 
     int rows;
     std::cin >> rows;
@@ -64,16 +73,7 @@ int main(int argc, char *argv[]){
         }
     }
 
-    if(mode == CLI_MODE_ALL){
-        currID = k;
-    }
-    else if(mode == CLI_MODE_RATE){
-        currID = 0;
-    }
-    else if(mode == CLI_MODE_THREAD){
-        currID = 1;
-    }
-
+    std::vector<Shared> out(rowsv.size());
     std::vector<Thread> threads(k);
 
     for (int i = 0; i < k; i++) {
@@ -81,13 +81,31 @@ int main(int argc, char *argv[]){
         threads[i].k = k;
         threads[i].rows = &rowsv;
         threads[i].currID = &currID;
-        threads[i].timeout_ms = (Timings_t)&timeout_ms;
-
+        threads[i].timeout_ms = timeout_ms;
+ 
         ::pthread_create(&thread_pool[i], nullptr, StartRoutine, &threads[i]);
+    }
+
+    if(mode == CLI_MODE_ALL){
+        currID = k;
+    }
+    else if(mode == CLI_MODE_RATE){
+        for(int relT = 1; relT <= k; relT++){
+            currID = relT;
+            Timings_SleepMs(1);
+        }
+    }
+    else if(mode == CLI_MODE_THREAD){
+        currID = 1;
     }
 
     for (int i = 0; i < k; i++) {
         ::pthread_join(thread_pool[i], nullptr);
+    }
+
+    ThreadLog("Thread   Start   Encryption");
+    for(int i = 0; i < (int)rowsv.size(); i++){
+        ThreadLog("%i        %i       %s", i, rowsv[i].id, (out[i].finished) ? out[i].digest : "timeout");
     }
 
     return 0;
@@ -97,13 +115,32 @@ void* StartRoutine(void* arg){
     Thread* x = static_cast<Thread*>(arg);
     int i = x->threadID;
 
+    //Checks if index goes beyond max threads given
+    if(i > x->k){
+        ThreadLog("[thread %i] returned", i);
+        return nullptr;
+    }
+
+    //Pause
+    while (i > *(x->currID)) {
+        Timings_SleepMs(1);
+    }
+
     ThreadLog("[thread %i] started", i);
+
+    //Racy
+    if(mode == CLI_MODE_THREAD && i < x->k){
+        *(x->currID) = i + 1;
+    }
+
     Timings_t startTime = Timings_NowMs();
 
-    while(i < *(x->currID) || i < x->k){ }
+    if (mode == CLI_MODE_THREAD && i < x->k) {
+        *(x->currID) = i + 1;
+    }
 
-    for(int ind = i; ind < (int)x->rows->size() && !Timings_TimeoutExpired(startTime, *(x->timeout_ms)); ind += x->k){
-
+    for(int ind = i; ind < (int)x->rows->size() && !Timings_TimeoutExpired(startTime, x->timeout_ms); ind += x->k){
+        ThreadLog("[thread %i] completed row %i", i, i - 1);
     }
 
     ThreadLog("[thread %i] returned", i);
